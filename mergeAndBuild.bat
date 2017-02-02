25be56d16cb2668cedc1654e155705fdc9173c61
svc-student <svc-student@student.directory.jccc>
2017-02-01 18:47:20 -0600

    added in the required stuff
    yeah, title

---
+++ mergeAndBuild.bat
@@ -0,0 +1,2 @@
+"C:\Program Files\Unity\Editor\Data\Tools\UnityYAMLMerge.exe" merge -p "Blank.unity" "Red.unity" "Blue.unity" "End.unity"
+"C:\Program Files\Unity\Editor\Unity.exe" -quit -batchmode -projectpath "C:\Users\svc-student\Desktop\UnityExample" -buildWindowsPlayer "C:\Users\svc-student\Desktop\UnityExample\Builds\windowsBuild.exe"
